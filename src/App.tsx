694b6e21aa8c7a5c15666cc627284870143f3cf7
lewiskyron <kyronnyoro@uni.minerva.edu>
2024-04-14 22:07:19 +0300

    page-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import './App.css'
-import './index.css'; // Adjust the path according to your project structure
-import PageSnapshotComponent from './components/Menu/PageSnapshot';
-
+import "./App.css";
+import "./index.css"; // Adjust the path according to your project structure
+import PageSnapshotComponent from "./components/Menu/PageSnapshot";
+import LoginForm from "./components/auth/login";
+// import { supabase } from "./utils/supabase";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-      <PageSnapshotComponent />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PageSnapshotComponent />}></Route>
+          <Route path="/auth" element={<LoginForm />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
