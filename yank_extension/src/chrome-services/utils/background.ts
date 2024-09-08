d5c39f1c33c976909d1ea799e847b897a289836a
lewiskyron <kyronnyoro@uni.minerva.edu>
2024-09-08 20:05:47 +0200

    setting_up vite extension & removing bloat

---
+++ yank_extension/src/chrome-services/utils/background.ts
@@ -0,0 +1,11 @@
+// background.ts
+
+console.log("Background script loaded");
+
+// Listen for installation event
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Extension installed");
+});
+
+// Export an empty object to make this a module
+export {};
--- yank_extension/vite.config.ts
+++ yank_extension/vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // @ts-expect-error - crx plugin doesn't have proper TypeScript definitions
+  plugins: [react(), crx({ manifest })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
