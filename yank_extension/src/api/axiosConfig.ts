012f4f0040b8012ac77a87724e6fd066cb41c206
Lewis Kyron <kyronnyoro@uni.minerva.edu>
2024-10-14 14:55:54 -0700

    LLM setup&testing

---
+++ yank_extension/src/api/axiosConfig.ts
@@ -0,0 +1,11 @@
+import axios, { AxiosInstance } from 'axios';
+
+const api: AxiosInstance = axios.create({
+    baseURL: 'https://api.groq.com/openai/v1/chat',
+    headers: {
+        'Content-Type' : 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_GROQ_API_KEY}`
+    },
+});
+
+export default api;
