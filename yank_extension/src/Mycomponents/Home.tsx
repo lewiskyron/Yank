c481fe3cea01122a1943d2cd59585624fc803d69
Lewis Kyron <kyronnyoro@uni.minerva.edu>
2024-10-15 16:29:39 -0700

    toggle bug fix

--- yank_extension/src/Mycomponents/Home.tsx
+++ yank_extension/src/Mycomponents/Home.tsx
@@ -3,14 +3,14 @@ import EditablePreview from "./EditablePreview.tsx";
 import { Tags } from "./tags.tsx";
 import { Button } from "@/components/ui/button";
 import React, { useEffect, useState } from "react";
-import api from "../api/axiosConfig.ts";
-import axios, { AxiosError } from "axios";
+// import api from "../api/axiosConfig.ts";
+// import axios, { AxiosError } from "axios";
 
 const Home: React.FC = () => {
 	const [selectedText, setSelectedText] = useState<string | null>(null);
-	const [responseText, setResponseText] = useState<string>("");
-	const [error, setError] = useState<string>("");
-	const [isloading, setIsloading] = useState<boolean>(false);
+	// const [responseText, setResponseText] = useState<string>("");
+	// const [error, setError] = useState<string>("");
+	// const [isloading, setIsloading] = useState<boolean>(false);
 
 	useEffect(() => {
 		const getHighlightedText = async () => {
@@ -22,71 +22,71 @@ const Home: React.FC = () => {
 					setSelectedText(response.data.text);
 				} else {
 					console.error("Error retrieving highlighted text:", response.message);
-					setError("Failed to retrieve highlighted text.");
+					// setError("Failed to retrieve highlighted text.");
 				}
 			} catch (err) {
 				console.error("Error in chrome.runtime.sendMessage:", err);
-				setError("An error occurred while fetching highlighted text.");
+				// setError("An error occurred while fetching highlighted text.");
 			}
 		};
 
 		getHighlightedText();
 	}, []);
 
-	useEffect(() => {
-		const fetchFlashCard = async () => {
-			if (selectedText && selectedText.trim() !== "") {
-				setIsloading(true);
-				try {
-					const customPrompt = `
-						Using the following text, create a flashcard with a clear and concise question and answer.
+	// useEffect(() => {
+	// 	const fetchFlashCard = async () => {
+	// 		if (selectedText && selectedText.trim() !== "") {
+	// 			setIsloading(true);
+	// 			try {
+	// 				const customPrompt = `
+	// 					Using the following text, create a flashcard with a clear and concise question and answer.
 
-						- **Format**:
-						- **Question**: A single, direct question that tests understanding of the key concept.
-						- **Answer**: A brief explanation or definition answering the question.
+	// 					- **Format**:
+	// 					- **Question**: A single, direct question that tests understanding of the key concept.
+	// 					- **Answer**: A brief explanation or definition answering the question.
 
-						Text:
-						"""${selectedText}"""
-						`;
-					const response = await api.post("/completions", {
-						messages: [
-							{
-								role: "user",
-								content: customPrompt,
-							},
-						],
-						model: "llama3-8b-8192",
-					});
-					const assistantMessage =
-						response.data.choices[0]?.message?.content || "";
-					setResponseText(assistantMessage);
-				} catch (err) {
-					if (axios.isAxiosError(err)) {
-						const axiosError = err as AxiosError;
-						console.error("Axios error:", axiosError.message);
-						setError(`API Error: ${axiosError.message}`);
-					} else {
-						console.error("Unexpected error:", err);
-						setError("An unexpected error occurred.");
-					}
-				} finally {
-					setIsloading(false);
-				}
-			}
-		};
-		fetchFlashCard();
-	}, [selectedText]);
+	// 					Text:
+	// 					"""${selectedText}"""
+	// 					`;
+	// 				const response = await api.post("/completions", {
+	// 					messages: [
+	// 						{
+	// 							role: "user",
+	// 							content: customPrompt,
+	// 						},
+	// 					],
+	// 					model: "llama3-8b-8192",
+	// 				});
+	// 				const assistantMessage =
+	// 					response.data.choices[0]?.message?.content || "";
+	// 				setResponseText(assistantMessage);
+	// 			} catch (err) {
+	// 				if (axios.isAxiosError(err)) {
+	// 					const axiosError = err as AxiosError;
+	// 					console.error("Axios error:", axiosError.message);
+	// 					setError(`API Error: ${axiosError.message}`);
+	// 				} else {
+	// 					console.error("Unexpected error:", err);
+	// 					setError("An unexpected error occurred.");
+	// 				}
+	// 			} finally {
+	// 				setIsloading(false);
+	// 			}
+	// 		}
+	// 	};
+	// 	fetchFlashCard();
+	// }, [selectedText]);
 
 	// Fetch data from OpenAI API whenever selectedText changes
 
 	return (
 		<div className="flex flex-col">
 			<Folders />
-			{isloading && <p>Loading...</p>}
-			{error && !isloading && <p style={{ color: "red" }}>{error}</p>}
-			{!isloading && (
-				<EditablePreview initialText={responseText ? responseText : ""} />
-			)}
+			{/* {isloading && <p>Loading...</p>}
+			{error && !isloading && <p style={{ color: "red" }}>{error}</p>} */}
+			{/* {!isloading && ( */}
+			<EditablePreview initialText={selectedText ? selectedText : ""} />
+			{/* )} */}
 			<Tags />
 			<Button className="mt-2 text-white">Save</Button>
 		</div>
