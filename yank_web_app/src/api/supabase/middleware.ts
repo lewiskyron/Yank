76657efbf1c650604b1166e9210930e097b34509
Lewis Kyron <kyronnyoro@uni.minerva.edu>
2024-12-27 13:12:07 -0800

    private_policy page

--- yank_web_app/src/api/supabase/middleware.ts
+++ yank_web_app/src/api/supabase/middleware.ts
@@ -2,6 +2,11 @@ import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function updateSession(request: NextRequest) {
+
+	if (request.nextUrl.pathname === '/PrivatePolicy') {
+        return NextResponse.next();
+    }
+	
 	let supabaseResponse = NextResponse.next({
 		request,
 	});
