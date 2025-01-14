6e7b865b0fc7bace68e680cf38cebc61367058a5
Lewis Kyron <kyronnyoro@uni.minerva.edu>
2025-01-13 17:59:04 -0800

    card-initialization edge function

---
+++ yank_web_app/src/types/database.types.ts
@@ -0,0 +1,245 @@
+export type Json =
+	| string
+	| number
+	| boolean
+	| null
+	| { [key: string]: Json | undefined }
+	| Json[];
+
+export type Database = {
+	public: {
+		Tables: {
+			Flashcard: {
+				Row: {
+					created_at: string;
+					difficulty: number | null;
+					due: string | null;
+					flashcard_id: string;
+					folder_id: number;
+					lapses: number | null;
+					last_review: string | null;
+					reps: number | null;
+					stability: number | null;
+					state: string | null;
+					text: string;
+					user_id: string | null;
+				};
+				Insert: {
+					created_at?: string;
+					difficulty?: number | null;
+					due?: string | null;
+					flashcard_id?: string;
+					folder_id: number;
+					lapses?: number | null;
+					last_review?: string | null;
+					reps?: number | null;
+					stability?: number | null;
+					state?: string | null;
+					text: string;
+					user_id?: string | null;
+				};
+				Update: {
+					created_at?: string;
+					difficulty?: number | null;
+					due?: string | null;
+					flashcard_id?: string;
+					folder_id?: number;
+					lapses?: number | null;
+					last_review?: string | null;
+					reps?: number | null;
+					stability?: number | null;
+					state?: string | null;
+					text?: string;
+					user_id?: string | null;
+				};
+				Relationships: [
+					{
+						foreignKeyName: "Flashcard_folder_id_fkey";
+						columns: ["folder_id"];
+						isOneToOne: false;
+						referencedRelation: "Folders";
+						referencedColumns: ["folder_id"];
+					},
+				];
+			};
+			Folders: {
+				Row: {
+					created_at: string;
+					folder_id: number;
+					folder_name: string;
+					no_of_flashcards: number;
+					user_id: string;
+				};
+				Insert: {
+					created_at?: string;
+					folder_id?: number;
+					folder_name: string;
+					no_of_flashcards?: number;
+					user_id?: string;
+				};
+				Update: {
+					created_at?: string;
+					folder_id?: number;
+					folder_name?: string;
+					no_of_flashcards?: number;
+					user_id?: string;
+				};
+				Relationships: [];
+			};
+			profiles: {
+				Row: {
+					avatar_url: string | null;
+					first_name: string | null;
+					id: string;
+					last_name: string | null;
+					role: string | null;
+					updated_at: string | null;
+				};
+				Insert: {
+					avatar_url?: string | null;
+					first_name?: string | null;
+					id: string;
+					last_name?: string | null;
+					role?: string | null;
+					updated_at?: string | null;
+				};
+				Update: {
+					avatar_url?: string | null;
+					first_name?: string | null;
+					id?: string;
+					last_name?: string | null;
+					role?: string | null;
+					updated_at?: string | null;
+				};
+				Relationships: [];
+			};
+			views: {
+				Row: {
+					created_at: string;
+					id: number;
+					namer: string | null;
+				};
+				Insert: {
+					created_at?: string;
+					id?: number;
+					namer?: string | null;
+				};
+				Update: {
+					created_at?: string;
+					id?: number;
+					namer?: string | null;
+				};
+				Relationships: [];
+			};
+		};
+		Views: {
+			[_ in never]: never;
+		};
+		Functions: {
+			[_ in never]: never;
+		};
+		Enums: {
+			[_ in never]: never;
+		};
+		CompositeTypes: {
+			[_ in never]: never;
+		};
+	};
+};
+
+type PublicSchema = Database[Extract<keyof Database, "public">];
+
+export type Tables<
+	PublicTableNameOrOptions extends
+		| keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+		| { schema: keyof Database },
+	TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+		? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+				Database[PublicTableNameOrOptions["schema"]]["Views"])
+		: never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+	? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+			Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+			Row: infer R;
+		}
+		? R
+		: never
+	: PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+				PublicSchema["Views"])
+		? (PublicSchema["Tables"] &
+				PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+				Row: infer R;
+			}
+			? R
+			: never
+		: never;
+
+export type TablesInsert<
+	PublicTableNameOrOptions extends
+		| keyof PublicSchema["Tables"]
+		| { schema: keyof Database },
+	TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+		? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+		: never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+	? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+			Insert: infer I;
+		}
+		? I
+		: never
+	: PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+		? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+				Insert: infer I;
+			}
+			? I
+			: never
+		: never;
+
+export type TablesUpdate<
+	PublicTableNameOrOptions extends
+		| keyof PublicSchema["Tables"]
+		| { schema: keyof Database },
+	TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+		? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+		: never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+	? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+			Update: infer U;
+		}
+		? U
+		: never
+	: PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+		? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+				Update: infer U;
+			}
+			? U
+			: never
+		: never;
+
+export type Enums<
+	PublicEnumNameOrOptions extends
+		| keyof PublicSchema["Enums"]
+		| { schema: keyof Database },
+	EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+		? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+		: never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+	? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+	: PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+		? PublicSchema["Enums"][PublicEnumNameOrOptions]
+		: never;
+
+export type CompositeTypes<
+	PublicCompositeTypeNameOrOptions extends
+		| keyof PublicSchema["CompositeTypes"]
+		| { schema: keyof Database },
+	CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+		schema: keyof Database;
+	}
+		? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+		: never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+	? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+	: PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
+		? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+		: never;
---
+++ yank_web_app/supabase/functions/card-init/index.ts
@@ -0,0 +1,78 @@
+/* eslint-disable */
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import type { Tables } from "../../../src/types/database.types.ts";
+import { createClient } from "npm:@supabase/supabase-js@2.45.6";
+import { Card, createEmptyCard } from "npm:ts-fsrs@4.6.0";
+
+console.log("Hello from Card-init Functions!");
+
+const supabase = createClient(
+	Deno.env.get("SUPABASE_URL"),
+	Deno.env.get("SUPABASE_SERVICE_ROLE_KEY"),
+);
+
+type CardRecord = Tables<"Flashcard">;
+
+interface CardPayload {
+	type: "INSERT" | "UPDATE" | "DELETE";
+	record: CardRecord;
+	schema: "public";
+}
+
+Deno.serve(async (req) => {
+	try {
+		if (req.method !== "POST") {
+			console.error("Invalid request method:", req.method);
+			return new Response(JSON.stringify({ error: "Method not allowed" }), {
+				status: 405,
+				headers: { "Content-Type": "application/json" },
+			});
+		}
+
+		const FlashcardRecordPayload: CardPayload = await req.json();
+		const { flashcard_id } = FlashcardRecordPayload.record;
+		console.log(flashcard_id);
+		const card: Card = createEmptyCard();
+
+		const responseData = {
+			state: card.state, // The current state of the card (New, Learning, Review, Relearning)
+			due: card.due, // Date when the card is next due for review
+			stability: card.stability, // A measure of how well the information is retained
+			difficulty: card.difficulty, // Reflects the inherent difficulty of the card content
+			scheduled_days: card.scheduled_days, // The interval at which the card is next scheduled
+			reps: card.reps, // Total number of times the card has been reviewed
+			lapses: card.lapses, // Times the card was forgotten or remembered incorrectly
+			last_review: card.last_review, // The most recent review date, if applicable
+		};
+
+		console.log(responseData);
+
+		const { error } = await supabase
+			.from("Flashcard")
+			.update(responseData)
+			.eq("flashcard_id", flashcard_id);
+
+		if (error) {
+			console.error("Database error:", error);
+			return new Response(JSON.stringify({ error: error.message }), {
+				status: 500,
+				headers: { "Content-Type": "application/json" },
+			});
+		}
+
+		return new Response(
+			JSON.stringify({ message: "Flashcard fsrs record updated successfully" }),
+			{
+				headers: {
+					"Content-Type": "application/json",
+				},
+			},
+		);
+	} catch (error) {
+		console.error("Unexpected error:", error);
+		return new Response(JSON.stringify({ error: "Internal server error" }), {
+			status: 500,
+			headers: { "Content-Type": "application/json" },
+		});
+	}
+});
