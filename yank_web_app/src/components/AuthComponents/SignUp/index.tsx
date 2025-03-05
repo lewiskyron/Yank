3c6c15ae12b5d86f4a387c50ad56406a596a5895
Lewis Kyron <kyronnyoro@uni.minerva.edu>
2025-03-04 23:51:12 -0800

    ommited email sign-in(to be returned eventually)

--- yank_web_app/src/components/AuthComponents/SignUp/index.tsx
+++ yank_web_app/src/components/AuthComponents/SignUp/index.tsx
@@ -8,7 +8,7 @@ export default function Signup() {
 		<>
 			<GoogleSigninButton text="Sign in" />
 
-			<div className="my-6 flex items-center justify-center">
+			{/* <div className="my-6 flex items-center justify-center">
 				<span className="bg-stroke dark:bg-dark-3 block h-px w-full"></span>
 				<div className="dark:bg-gray-dark block w-full min-w-fit bg-white px-3 text-center font-medium">
 					Or sign in with email
@@ -18,7 +18,7 @@ export default function Signup() {
 
 			<div>
 				<SigninWithPassword mode="signUp" />
-			</div>
+			</div> */}
 		</>
 	);
 }
--- yank_web_app/src/components/AuthComponents/Signin/index.tsx
+++ yank_web_app/src/components/AuthComponents/Signin/index.tsx
@@ -11,20 +11,20 @@ export default function Signin() {
 
 			<div className="my-6 flex items-center justify-center">
 				<span className="bg-stroke dark:bg-dark-3 block h-px w-full"></span>
-				<div className="dark:bg-gray-dark block w-full min-w-fit bg-white px-3 text-center font-medium">
+				{/* <div className="dark:bg-gray-dark block w-full min-w-fit bg-white px-3 text-center font-medium">
 					Or sign in with email
-				</div>
+				</div> */}
 				<span className="bg-stroke dark:bg-dark-3 block h-px w-full"></span>
 			</div>
 
-			<div>
+			{/* <div>
 				<SigninWithPassword mode="login" />
-			</div>
+			</div> */}
 
 			<div className="mt-6 text-center">
 				<p>
-					Don’t have any account?{" "}
-					<Link href="/auth/signUp" className="text-primary">
+					Don't have any account?{" "}
+					<Link href="/auth/signUp" className="font-medium text-blue-400">
 						Sign Up
 					</Link>
 				</p>
